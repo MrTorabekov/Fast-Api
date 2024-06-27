f499ca2e808065cbcf9ec1ad92064d069928e731
MrTorabekov <diyorbektorabekov08@gmail.com>
2024-06-27 08:51:05 +0500

    commit

--- dataApi.py
+++ dataApi.py
@@ -4,17 +4,17 @@ from main import translate_text, trans_text
 app = FastAPI()
 
 
-@app.get('/uz_en/{name}')
-async def uz_en(name: str):
-    data = translate_text(name)
+@app.get('/uz_en/{text}')
+async def uz_en(text: str):
+    data = translate_text(text)
     return {
         "ENG": data
     }
 
 
-@app.get('/en_uz/{namee}')
-async def en_uz(namee: str):
-    data = trans_text(namee)
+@app.get('/en_uz/{text}')
+async def en_uz(text: str):
+    data = trans_text(text)
     return {
         "UZ": data
     }
